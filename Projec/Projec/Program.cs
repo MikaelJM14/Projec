c3efa97898a05aa39e8d3c391607f10aa2f0d16a
mikael <mikael.programing.c@gmail.com>
2024-06-09 14:20:54 +0100

    proj

---
+++ Projec/Projec/Program.cs
@@ -0,0 +1,17 @@
+﻿var FileName = "MAT.txt";
+
+using (var writer = File.AppendText(FileName))
+{
+    writer.WriteLine("best");
+}
+if (File.Exists(FileName))
+{
+    using (var reader = File.OpenText(FileName))
+    {
+        var line = reader.ReadLine();
+        while (line != null)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
